c2becbbb2295add3559dc7962e4c07e883e327af
Julian <julian-ghb@hotmail.com>
2023-03-08 18:07:42 -0300

    reescrevendoComListas

--- main.js
+++ main.js
@@ -1,44 +1,15 @@
-let teclaClicada = [document.querySelector('.tecla_pom'),
-                    document.querySelector('.tecla_clap'),
-                    document.querySelector('.tecla_tim'), 
-                    document.querySelector('.tecla_puff'),
-                    document.querySelector('.tecla_splash'),
-                    document.querySelector('.tecla_toim'),
-                    document.querySelector('.tecla_psh'),
-                    document.querySelector('.tecla_tic'),
-                    document.querySelector('.tecla_tom'),]
+const teclaClicada = document.querySelectorAll('.tecla');
 
 for (let i = 0; i < teclaClicada.length; i++) {
-    teclaClicada[i].onclick = tocaSomPom;
     
-}
+    const tecla = teclaClicada[i];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`;
 
-
-
-function tocaSomPom() {
-    document.querySelector('#som_tecla_pom').play();
+    tecla.onclick = function () {
+        tocaSom(idAudio);
+    }
 }
-function tocaSomClap() {
-    document.querySelector('#som_tecla_clap').play();
-}
-function tocaSomTim() {
-    document.querySelector('#som_tecla_tim').play();
-}
-function tocaSomPuff() {
-    document.querySelector('#som_tecla_puff').play();
-}
-function tocaSomSplash() {
-    document.querySelector('#som_tecla_splash').play();
-}
-function tocaSomToim() {
-    document.querySelector('#som_tecla_toim').play();
-}
-function tocaSomPsh() {
-    document.querySelector('#som_tecla_psh').play();
-}
-function tocaSomTic() {
-    document.querySelector('#som_tecla_tic').play();
-}
-function tocaSomTom() {
-    document.querySelector('#som_tecla_tom').play();
+function tocaSom(IdElementAudio) {
+    document.querySelector(IdElementAudio).play();
 }
