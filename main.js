9ea956cb15cc9430bd687bb7cffab07e5066d1c9
Julian <julian-ghb@hotmail.com>
2023-03-08 14:48:02 -0300

    botoes

--- main.js
+++ main.js
@@ -6,23 +6,39 @@ let teclaClicada = [document.querySelector('.tecla_pom'),
                     document.querySelector('.tecla_toim'),
                     document.querySelector('.tecla_psh'),
                     document.querySelector('.tecla_tic'),
-                    document.querySelector('.tecla_tom'),
-                    ]
+                    document.querySelector('.tecla_tom'),]
 
 for (let i = 0; i < teclaClicada.length; i++) {
-    teclaClicada[i].onclick = som[i];
+    teclaClicada[i].onclick = tocaSomPom;
     
 }
 
-let som = [document.querySelector('#som_tecla_pom').play(),
-           document.querySelector('#som_tecla_clap').play(),
-           document.querySelector('#som_tecla_tim').play(),
-           document.querySelector('#som_tecla_puff').play(),
-           document.querySelector('#som_tecla_splash').play(),
-           document.querySelector('#som_tecla_toim').play(),
-           document.querySelector('#som_tecla_psh').play(),
-           document.querySelector('#som_tecla_tic').play(),
-           document.querySelector('#som_tecla_tom').play(),
-           ]
 
 
+function tocaSomPom() {
+    document.querySelector('#som_tecla_pom').play();
+}
+function tocaSomClap() {
+    document.querySelector('#som_tecla_clap').play();
+}
+function tocaSomTim() {
+    document.querySelector('#som_tecla_tim').play();
+}
+function tocaSomPuff() {
+    document.querySelector('#som_tecla_puff').play();
+}
+function tocaSomSplash() {
+    document.querySelector('#som_tecla_splash').play();
+}
+function tocaSomToim() {
+    document.querySelector('#som_tecla_toim').play();
+}
+function tocaSomPsh() {
+    document.querySelector('#som_tecla_psh').play();
+}
+function tocaSomTic() {
+    document.querySelector('#som_tecla_tic').play();
+}
+function tocaSomTom() {
+    document.querySelector('#som_tecla_tom').play();
+}
